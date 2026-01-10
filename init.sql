64dc82128d52d0f2a6a2b4ea5878c7363c83aea6
usspecialforces713-cmd <usspecialforces713@gmail.com>
2026-01-10 18:50:51 +0100

    Create users table in init.sql

---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL,
+    password TEXT NOT NULL,
+    reason TEXT,
+    photo TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
