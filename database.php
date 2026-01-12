0abb6da06d580b6afc63217eac9345547833b831
usspecialforces713-cmd <usspecialforces713@gmail.com>
2026-01-12 12:17:08 +0100

    Update database.php

--- database.php
+++ database.php
@@ -1,20 +1,19 @@
 <?php
-$host = getenv("hostname");
-$port = getenv("Port");
-$db   = getenv("database");
-$user = getenv("username");
-$pass = getenv("pasword");
+$host = getenv("PGHOST");
+$port = getenv("PGPORT");
+$db   = getenv("PGDATABASE");
+$user = getenv("PGUSER");
+$pass = getenv("PGPASSWORD");
 
 try {
     $pdo = new PDO(
         "pgsql:host=$host;port=$port;dbname=$db",
         $user,
         $pass,
-        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]
     );
-   
 } catch (PDOException $e) {
-    die("Erreur : " . $e->getMessage());
+    die("Erreur connexion DB : " . $e->getMessage());
 }
-?>
-
